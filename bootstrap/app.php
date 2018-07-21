f015febb8864e6aa566a54e335c9935716806524
Johannes Hejslet Jørgensen <johannes@hejslet.dk>
2018-07-21 17:48:16 +0200

    Enable Eloquent

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ $app = new Laravel\Lumen\Application(
 
 // $app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -66,6 +66,10 @@ $app->singleton(
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
+
+$app->middleware([
+  App\Http\Middleware\CorsMiddleware::class
+]);
 
 /*
 |--------------------------------------------------------------------------
